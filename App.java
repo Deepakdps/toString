5a2a1c70e766fd8ff5b27a183b5b8bf5e5405a46
deepak <deepak.seby@gmail.com>
2017-08-23 11:29:31 +0530

    my first init

---
+++ App.java
@@ -0,0 +1,38 @@
+package pattern;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		for (int row = 1; row <= 4; row++) {
+
+			for (int colspaces = 4 - row; colspaces >= 1; colspaces--) {
+
+				System.out.print(" ");
+			}
+
+			for (int colStar = 1; colStar <= row; colStar++) {
+
+				System.out.print(row+" ");
+
+			}
+
+			System.out.println("\n");
+
+		}
+	}
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
